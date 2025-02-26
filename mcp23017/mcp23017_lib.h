38e04a6de01164d4b0276618d1be1af9b1100500
Derya Y. <iiot2k@gmail.com>
2025-02-26 21:23:40 +0100

    Add files via upload

--- mcp23017/mcp23017_lib.h
+++ mcp23017/mcp23017_lib.h
@@ -48,7 +48,7 @@ enum {
  */
 enum {
     INPUT = 0, 		// pin is input (default on reset)
-    INPUT_INV,		// pin is inverted input
+    INPUT_INV,      // pin is inverted input
     INPUT_PULL,		// pin is input with pullup-resistor
     INPUT_PULL_INV,	// pin is input inverted with pullup-resistor
     OUTPUT, 		// pin is output set inactive
@@ -69,25 +69,25 @@ enum {
 const char* error_text();
 
 /**
- * @brief sets mcp23017 port mode for each pin
+ * @brief sets mcp23017/mcp23008 port mode for each pin
  * @param i2c_port i2c port (0..9)
  * @param adr i2c address (ADR_..)
  * @param port_adr selects port (PORT_A ..)
- * @param mode_arr mode values (INPUT..) as array
+ * @param mode mode values (INPUT..) as array
  * @returns true: ok, false: error
  *
  * @note INT pin is set to open-drain (set on active to low)
  * @note resets also interrupt
  *
- * @note if port_adr = PORT_A modes_arr array where index 0=GPA0..7=GPA7
- * @note if port_adr = PORT_B modes_arr array where index 0=GPB0..7=GPB7
- * @note if port_adr = PORT_AB modes_arr array where index 0=GPA0..7=GPA7, 8=GPB0 15=GPB7
- * @note if port_adr = PORT_8 modes_arr array where index 0=GP0..7=GP7 (mcp23008)
+ * @note if port_adr = PORT_A mode is array where index 0=GPA0..7=GPA7
+ * @note if port_adr = PORT_B mode is array where index 0=GPB0..7=GPB7
+ * @note if port_adr = PORT_AB mode is array where index 0=GPA0..7=GPA7, 8=GPB0 15=GPB7
+ * @note if port_adr = PORT_8 mode is array where index 0=GP0..7=GP7 (mcp23008)
  */
-bool set_mode(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, vector<uint8_t>& mode_arr);
+bool set_mode(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, vector<uint8_t>& mode);
 
 /**
- * @brief sets mcp23017 port mode for all pins
+ * @brief sets mcp23017/mcp23008 port mode for all pins
  * @param i2c_port i2c port (0..9)
  * @param adr i2c address (ADR_..)
  * @param port_adr selects port (PORT_A ..)
@@ -105,25 +105,25 @@ bool set_mode(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, vector<uint8_t>& 
 bool set_mode(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, uint8_t mode);
 
 /**
- * @brief sets mcp23017 interrupt mode for each pin
+ * @brief sets mcp23017/mcp23008 interrupt mode for each pin
  * @param i2c_port i2c port (0..9)
  * @param adr i2c address (ADR_..)
  * @param port_adr selects port (PORT_A ..)
- * @param mode_arr interrupt mode values (INT_OFF, INT_ON) as array
+ * @param mode interrupt mode values (INT_OFF, INT_ON) as array
  * @returns true: ok, false: error
  *
  * @note INT pin is set to open-drain (set on active to low)
  * @note resets also interrupt
  *
- * @note if port_adr = PORT_A modes_arr array where index 0=GPA0..7=GPA7
- * @note if port_adr = PORT_B modes_arr array where index 0=GPB0..7=GPB7
- * @note if port_adr = PORT_AB modes_arr array where index 0=GPA0..7=GPA7, 8=GPB0 15=GPB7
- * @note if port_adr = PORT_8 modes_arr array where index 0=GP0..7=GP7 (mcp23008)
+ * @note if port_adr = PORT_A mode is array where index 0=GPA0..7=GPA7
+ * @note if port_adr = PORT_B mode is array where index 0=GPB0..7=GPB7
+ * @note if port_adr = PORT_AB mode is array where index 0=GPA0..7=GPA7, 8=GPB0 15=GPB7
+ * @note if port_adr = PORT_8 mode is array where index 0=GP0..7=GP7 (mcp23008)
  */
-bool set_interrupt_mode(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, vector<uint8_t>& mode_arr);
+bool set_interrupt_mode(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, vector<uint8_t>& mode);
 
 /**
- * @brief sets mcp23017 interrupt mode for all pins
+ * @brief sets mcp23017/mcp23008 interrupt mode for all pins
  * @param i2c_port i2c port (0..9)
  * @param adr i2c address (ADR_..)
  * @param port_adr selects port (PORT_A ..)
@@ -141,7 +141,7 @@ bool set_interrupt_mode(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, vector<
 bool set_interrupt_mode(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, uint8_t mode);
 
 /**
- * @brief reads mcp23017 interrupt state and returns array
+ * @brief reads mcp23017/mcp23008 interrupt state and returns array
  * @param i2c_port i2c port (0..9)
  * @param adr i2c address (ADR_..)
  * @param port_adr selects port (PORT_A ..)
@@ -150,15 +150,15 @@ bool set_interrupt_mode(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, uint8_t
  *
  * @note if array element is 1 then interrupt occurs on that pin
  *
- * @note if port_adr = PORT_A int_data array where index 0=GPA0..7=GPA7
- * @note if port_adr = PORT_B int_data array where index 0=GPB0..7=GPB7
- * @note if port_adr = PORT_AB int_data array where index 0=GPA0..7=GPA7, 8=GPB0 15=GPB7
- * @note if port_adr = PORT_8 int_data array where index 0=GP0..7=GP7 (mcp23008)
+ * @note if port_adr = PORT_A int_state is array where index 0=GPA0..7=GPA7
+ * @note if port_adr = PORT_B int_state is array where index 0=GPB0..7=GPB7
+ * @note if port_adr = PORT_AB int_state is array where index 0=GPA0..7=GPA7, 8=GPB0 15=GPB7
+ * @note if port_adr = PORT_8 int_state is array where index 0=GP0..7=GP7 (mcp23008)
  */
 bool get_interrupt_state(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, vector<uint8_t>& int_state);
 
 /**
- * @brief reads mcp23017 interrupt state and returns number
+ * @brief reads mcp23017/mcp23008 interrupt state and returns number
  * @param i2c_port i2c port (0..9)
  * @param adr i2c address (ADR_..)
  * @param port_adr selects port (PORT_A ..)
@@ -167,15 +167,15 @@ bool get_interrupt_state(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, vector
  *
  * @note if bit is 1 then interrupt occurs on that pin
  *
- * @note if port_adr = PORT_A int_data is bit0=GPA0..bit7=GPA7
- * @note if port_adr = PORT_B int_data is bit0=GPB0..bit7=GPB7
- * @note if port_adr = PORT_AB int_data is bit0=GPA0..bit7=GPA7, bit8=GPB0..bit15=GPB7
- * @note if port_adr = PORT_8 int_data is bit0=GP0..bit7=GP7 (mcp23008)
+ * @note if port_adr = PORT_A int_state is bit0=GPA0..bit7=GPA7
+ * @note if port_adr = PORT_B int_state is bit0=GPB0..bit7=GPB7
+ * @note if port_adr = PORT_AB int_state is bit0=GPA0..bit7=GPA7, bit8=GPB0..bit15=GPB7
+ * @note if port_adr = PORT_8 int_state is bit0=GP0..bit7=GP7 (mcp23008)
  */
 bool get_interrupt_state(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, uint16_t& int_state);
 
 /**
- * @brief reads mcp23017 port and returns array
+ * @brief reads mcp23017/mcp23008 port and returns array
  * @param i2c_port i2c port (0..9)
  * @param adr i2c address (ADR_..)
  * @param port_adr selects port (PORT_A ..)
@@ -185,15 +185,15 @@ bool get_interrupt_state(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, uint16
  * @note if array element is 1 then pin is high
  * @note resets also interrupt
  *
- * @note if port_adr = PORT_A port_data array where index 0=GPA0..7=GPA7
- * @note if port_adr = PORT_B port_data array where index 0=GPB0..7=GPB7
- * @note if port_adr = PORT_AB port_data array 16 where index 0=GPA0..7=GPA7, 8=GPB0 15=GPB7
- * @note if port_adr = PORT_8 port_data array where index 0=GP0..7=GP7 (mcp23008)
+ * @note if port_adr = PORT_A port_data is array where index 0=GPA0..7=GPA7
+ * @note if port_adr = PORT_B port_data is array where index 0=GPB0..7=GPB7
+ * @note if port_adr = PORT_AB port_data is array where index 0=GPA0..7=GPA7, 8=GPB0 15=GPB7
+ * @note if port_adr = PORT_8 port_data is array where index 0=GP0..7=GP7 (mcp23008)
  */
 bool read_port(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, vector<uint8_t>& port_data);
 
 /**
- * @brief reads mcp23017 port and returns number
+ * @brief reads mcp23017/mcp23008 port and returns number
  * @param i2c_port i2c port (0..9)
  * @param adr i2c address (ADR_..)
  * @param port_adr selects port (PORT_A ..)
@@ -211,7 +211,7 @@ bool read_port(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, vector<uint8_t>&
 bool read_port(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, uint16_t& port_data);
 
 /**
- * @brief writes to mcp23017 port from array
+ * @brief writes to mcp23017/mcp23008 port from array
  * @param i2c_port i2c port (0..9)
  * @param adr i2c address (ADR_..)
  * @param port_adr selects port (PORT_A ..)
@@ -220,15 +220,15 @@ bool read_port(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, uint16_t& port_d
  *
  * @note if array element is 1 then pin is high
  *
- * @note if port_adr = PORT_A port_data array where index 0=GPA0..7=GPA7
- * @note if port_adr = PORT_B port_data array where index 0=GPB0..7=GPB7
- * @note if port_adr = PORT_AB port_data array where index 0=GPA0..7=GPA7, 8=GPB0 15=GPB7
- * @note if port_adr = PORT_8 port_data array where index 0=GP0..7=GP7 (mcp23008)
+ * @note if port_adr = PORT_A port_data is array where index 0=GPA0..7=GPA7
+ * @note if port_adr = PORT_B port_data is array where index 0=GPB0..7=GPB7
+ * @note if port_adr = PORT_AB port_data is array where index 0=GPA0..7=GPA7, 8=GPB0 15=GPB7
+ * @note if port_adr = PORT_8 port_data is array where index 0=GP0..7=GP7 (mcp23008)
  */
 bool write_port(uint8_t i2c_port, uint8_t adr, uint8_t port_adr, vector<uint8_t>& port_data);
 
 /**
- * @brief writes to mcp23017 port from number
+ * @brief writes to mcp23017/mcp23008 port from number
  * @param i2c_port i2c port (0..9)
  * @param adr i2c address (ADR_..)
  * @param port_adr selects port (PORT_A ..)
